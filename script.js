2b977c035a12d426e4379de00533bcfefe0fc139
arzatf <arzamansoori786@gmail.com>
2022-11-03 12:08:14 +0530

    adding style in website

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ const btn = document.getElementById("butt");
 const totalCount = document.getElementById("total")
 const totalCountS = document.getElementById("totalSpace")
 
-// totalCount.innerHTML = 0;
-
-// btn.addEventListener("input",()=>{
-//     totalCount.innerHTML = word.value.length
-// })
 
 word.oninput = function() {
     totalCount.innerHTML =`Total words including space : ${word.value.length}`
